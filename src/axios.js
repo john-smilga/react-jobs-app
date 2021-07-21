f8fa1d461df75b0a58138d4d25ac7a72372b4bcd
john-smilga <johnsmilga@gmail.com>
2021-07-21 09:24:12 -0700

    switched to heroku url

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
-axios.defaults.baseURL = 'https://www.course-api.com/jobs/api/v1'
+import axios from 'axios';
+axios.defaults.baseURL = 'https://jobs-api-06.herokuapp.com/api/v1';
 
 axios.interceptors.request.use(function (req) {
-  const user = localStorage.getItem('user')
+  const user = localStorage.getItem('user');
 
   if (user) {
-    const { token } = JSON.parse(localStorage.getItem('user'))
-    req.headers.authorization = `Bearer ${token}`
-    return req
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    req.headers.authorization = `Bearer ${token}`;
+    return req;
   }
-  return req
-})
+  return req;
+});
