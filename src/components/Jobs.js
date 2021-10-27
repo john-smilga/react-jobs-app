3e899964f226102938afb4f3a1a0aa1dfcb51db2
john-smilga <johnsmilga@gmail.com>
2021-10-27 12:01:35 -0700

    complete project

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -138,7 +138,13 @@ const Container = styled.section`
     justify-content: center;
     gap: 0 0.5rem;
   }
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+  }
   @media (min-width: 992px) {
+    grid-template-columns: 1fr;
     .icon {
       display: none;
     }
