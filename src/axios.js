5ca8d21c2a093e99b277ca0eda86314d83a7515a
john smilga <johnsmilga@gmail.com>
2022-10-31 11:14:45 -0400

    switched the api to render

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://jobs-api-06.herokuapp.com/api/v1';
+axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem('user');
